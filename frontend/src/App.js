3f6d936439c262001957de82b41b17e384faeffc
R0yJing <roy.jing@outlook.com>
2021-08-25 22:40:01 +1200

    changed App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello ZRSquared!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://trello.com/b/ha0hIDQw/ciseassessment1bteam-3"
           target="_blank"
           rel="noopener noreferrer"
         >
